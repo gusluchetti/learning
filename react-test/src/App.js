46112e9fc405ac42d585ed65c59251ac1ee4e619
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-09 23:25:39 -0300

    bun basic setup

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,26 +1,21 @@
 const Item = (props) => {
-    return React.createElement(
-        "div", 
-        null,
-        [
-        React.createElement("h2", null, props.title),
-        React.createElement("p", null, props.info),
-        React.createElement("p", null, props.extra),
-        ]
-    );
+  return React.createElement("div", null, [
+    React.createElement("h2", null, props.title),
+    React.createElement("p", null, props.info),
+    React.createElement("p", null, props.extra),
+  ]);
 };
 
-
 const App = () => {
-    return React.createElement(
-        "div", 
-        null,
-        [
-        React.createElement("h1", null, 'Hello Basic React!'),
-        React.createElement(Item, {title: 'test', info: 'nothing', extra: ''}),
-        React.createElement(Item, {title: 'spooky', info: 'halloween', extra: '?'}),
-        ]
-    );
+  return React.createElement("div", null, [
+    React.createElement("h1", null, "Hello Basic React!"),
+    React.createElement(Item, { title: "test", info: "nothing", extra: "" }),
+    React.createElement(Item, {
+      title: "spooky",
+      info: "halloween",
+      extra: "?",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
