87a91c8c761f11983512bdc497ebc6babbad041f
Gustavo Luchetti <gustavo@luchetti.dev>
2023-11-22 09:05:48 -0300

    feat: setup biome and run format

--- react-test/src/Item.jsx
+++ react-test/src/Item.jsx
@@ -1,11 +1,11 @@
 const Item = (props) => {
-  return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.info}</p>
-      <p>{props.extra}</p>
-    </div>
-  );
+	return (
+		<div>
+			<h2>{props.title}</h2>
+			<p>{props.info}</p>
+			<p>{props.extra}</p>
+		</div>
+	);
 };
 
 export default Item;
