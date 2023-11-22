cd83339feca2594c168ccceaf68ec5a3d673ceee
Gustavo Luchetti <gustavo@luchetti.dev>
2023-11-22 09:20:44 -0300

    feat: update biome config

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -2,13 +2,13 @@ import { createRoot } from "react-dom";
 import Item from "./Item";
 
 const App = () => {
-	return (
-		<div>
-			<h1>Hello Basic React!</h1>
-			<Item title="title" info="info" extra="extra" />
-			<Item title="2" info="testing" extra="things" />
-		</div>
-	);
+  return (
+    <div>
+      <h1>Hello Basic React!</h1>
+      <Item title="title" info="info" extra="extra" />
+      <Item title="2" info="testing" extra="things" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
