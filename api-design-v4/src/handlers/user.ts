280c82674160650d1baa121b7abb9d8416b45803
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-18 23:23:29 -0300

    feat: create new user and signin routes

---
+++ api-design-v4/src/handlers/user.ts
@@ -0,0 +1,35 @@
+import prisma from "../db";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
+
+export const createUser = async (req: any, res) => {
+  const hashed = await hashPassword(req.body.password);
+  if (!hashed) {
+    res.status(500).send('whoops!')
+    return;
+  }
+
+  const user = await prisma.users.create({
+    data: {
+      username: req.body.username,
+      password: hashed
+    }
+  })
+
+  const token = createJWT(user);
+  res.json({ token });
+}
+
+export const signIn = async (req, res) => {
+  const user = await prisma.users.findUnique({
+    where: { username: req.body.username }
+  })
+
+  const isValidUser = await comparePasswords(req.body.password, user.password);
+  if (!isValidUser) {
+    res.status(401).send('Wrong username/password')
+    return;
+  }
+
+  const token = createJWT(user);
+  res.json({ token });
+}
