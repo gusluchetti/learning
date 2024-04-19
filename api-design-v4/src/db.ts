96140a69d1a6df0c1321ea2c12b4f3538d342f2d
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-19 17:40:41 -0300

    chore: add note

--- api-design-v4/src/db.ts
+++ api-design-v4/src/db.ts
@@ -2,3 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export default prisma;
+
+// TODO: how to deal with the fact that db is down?
+// is total api shutdown ok?
