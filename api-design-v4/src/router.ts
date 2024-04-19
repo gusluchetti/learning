0e3dbbe31945a0ab929c66f402b5cebe0b259aa8
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-19 17:41:06 -0300

    feat: start populating routes

--- api-design-v4/src/router.ts
+++ api-design-v4/src/router.ts
@@ -1,21 +1,28 @@
 import { Router } from 'express'
+import { body, validationResult } from 'express-validator';
+import prisma from './db';
 
 const router = Router()
+// reminder: put is completely replace, patch is just change what was sent
 
 // posts
-router.get('/posts', (req, res) => {
-  res.json(['post1', 'post2'])
-  res.end()
-})
+router.get('/posts', () => { })
 router.post('/post', () => { })
-router.get('/post:id', () => { })
-router.put('/post:id', () => { }) // COMPLETELY REPLACE
-router.delete('/post:id', () => { })
+router.get('/post/:id', () => { })
+router.put('/post/:id', () => { })
+router.delete('/post/:id', () => { })
 
 // comments
-router.get('/comments:post_id', () => { })
+router.get('/comments/:post_id', () => { })
 router.post('/comment', () => { })
-router.put('/comment:id', () => { })
+router.put('/comment/:id', body('comment'), (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() })
+  }
+  res.end()
+})
 router.delete('/comment:id', () => { })
 
 export default router
