fc4508f4fd6d9f0ea83d608b3e4f793265324521
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-19 17:55:58 -0300

    feat: validation for other posts/patch routes

--- api-design-v4/src/router.ts
+++ api-design-v4/src/router.ts
@@ -7,14 +7,40 @@ const router = Router()
 
 // posts
 router.get('/posts', () => { })
-router.post('/post', () => { })
+
+router.post('/post',
+  body('title').notEmpty().isString(),
+  body('body').notEmpty().isString(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400);
+      res.json({ errors: errors.array() })
+    }
+    res.end()
+  })
+
 router.get('/post/:id', () => { })
-router.put('/post/:id', () => { })
+router.patch('/post/:id',
+  body('title').optional().isString(),
+  body('body').optional().isString(),
+  () => { })
 router.delete('/post/:id', () => { })
 
 // comments
 router.get('/comments/:post_id', () => { })
-router.post('/comment', () => { })
+
+router.post('/comment',
+  body('pid').notEmpty().isNumeric(),
+  body('comment').notEmpty().isString(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400);
+      res.json({ errors: errors.array() })
+    }
+    res.end()
+  })
 
 router.put('/comment/:id',
   body('comment').notEmpty().isString(),
