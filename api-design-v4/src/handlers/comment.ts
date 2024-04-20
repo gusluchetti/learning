65ee97339e0b43d8dc99b47585941671e9de5ac5
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-20 16:56:49 -0300

    feat: async error handler
    only doing it for comments for now

--- api-design-v4/src/handlers/comment.ts
+++ api-design-v4/src/handlers/comment.ts
@@ -1,45 +1,64 @@
 import prisma from '../db'
 import { Request, Response } from 'express';
 
-export const createComment = async (req: Request, res: Response) => {
-  const created = await prisma.comments.create({
-    data: {
-      post_id: req.body.pid,
-      comment: req.body.comment,
-    }
-  })
-  return res.json({ data: created });
+export const createComment = async (req: Request, res: Response, next) => {
+  try {
+    const created = await prisma.comments.create({
+      data: {
+        post_id: req.body.pid,
+        comment: req.body.comment,
+      }
+    })
+    return res.json({ data: created });
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const getComments = async (_: Request, res: Response) => {
-  const comments = await prisma.comments.findMany();
-  return res.json({ data: comments });
+export const getComments = async (_: Request, res: Response, next) => {
+  try {
+    const comments = await prisma.comments.findMany();
+    return res.json({ data: comments });
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const getComment = async (req: Request, res: Response) => {
-  let numId = parseInt(req.params.id);
-  const comment = await prisma.comments.findUnique({
-    where: { cid: numId }
-  });
-  return res.json({ data: comment });
+export const getComment = async (req: Request, res: Response, next) => {
+  try {
+    let numId = parseInt(req.params.id);
+    const comment = await prisma.comments.findUnique({
+      where: { cid: numId }
+    });
+    return res.json({ data: comment });
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const updateComment = async (req: Request, res: Response) => {
-  let numId = parseInt(req.params.id);
-  const update = await prisma.comments.update({
-    where: { cid: numId },
-    data: {
-      comment: req.params.comment || undefined,
-    }
-  })
-
-  return res.json({ data: update });
+export const updateComment = async (req: Request, res: Response, next) => {
+  try {
+    let numId = parseInt(req.params.id);
+    const update = await prisma.comments.update({
+      where: { cid: numId },
+      data: {
+        comment: req.params.comment || undefined,
+      }
+    })
+    return res.json({ data: update });
+  } catch (error) {
+    next(error);
+  }
 }
 
-export const deleteComment = async (req: Request, res: Response) => {
-  let numId = parseInt(req.params.id);
-  const comment = await prisma.comments.delete({
-    where: { cid: numId }
-  });
-  return res.json({ data: comment });
+export const deleteComment = async (req: Request, res: Response, next) => {
+  try {
+    let numId = parseInt(req.params.id);
+    const comment = await prisma.comments.delete({
+      where: { cid: numId }
+    });
+    return res.json({ data: comment });
+  } catch (error) {
+    next(error);
+  }
 }
