44bcc65fb7ec3fabb5ff9be70eb2d3d72619ea0e
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-20 20:20:44 -0300

    test: user testing
    wip

--- api-design-v4/src/handlers/tests/user.test.ts
+++ api-design-v4/src/handlers/tests/user.test.ts
@@ -1,5 +1,14 @@
+import * as user from '../user'
+
 describe('user handler', () => {
-  it('should work!', () => {
-    expect(1).toBe(1);
+  it('should create new user', async () => {
+    const req = { body: { username: 'hello', password: 'hi' } }
+    const res = {
+      json({ token }) {
+        expect(token).toBeTruthy()
+      }
+    }
+
+    const newUser = await user.createUser(req, res)
   })
 })
