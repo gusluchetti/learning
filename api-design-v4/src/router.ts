f1d7f0d25a0431efb5ac7c1cef9d72878fb4c928
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-20 16:12:59 -0300

    feat: use comment and post handlers on separate file

--- api-design-v4/src/router.ts
+++ api-design-v4/src/router.ts
@@ -2,85 +2,44 @@ import { Router } from 'express'
 import { body } from 'express-validator';
 import { handleInputErrors } from './modules/middleware';
 
-import prisma from './db';
+import { createPost, deletePost, getPost, getPosts, updatePost } from './handlers/post';
+import { createComment, deleteComment, getComment, updateComment } from './handlers/comment';
 
 const router = Router()
 // reminder: put is completely replace, patch is just change what was sent
 
 // posts
-router.get('/posts', async (req, res) => {
-  const posts = await prisma.posts.findMany();
-  return res.json(posts);
-})
-router.get('/post/:id', async (req, res) => {
-  let numId = parseInt(req.params.id);
-  const post = await prisma.posts.findUnique({
-    where: { pid: numId }
-  });
-  return res.json(post);
-})
+router.get('/post/:id', getPost)
+router.get('/posts', getPosts)
 
 router.post('/post',
   body('title').notEmpty().isString(),
   body('body').notEmpty().isString(),
-  handleInputErrors,
-  async (req, res) => {
-    const createPost = await prisma.posts.create({
-      data: {
-        title: req.body.title,
-        body: req.body.body,
-      }
-    })
-
-    return res.json(createPost);
-  });
+  handleInputErrors, createPost);
 
 router.patch('/post/:id',
   body('title').optional().isString(),
   body('body').optional().isString(),
-  handleInputErrors,
-  (req, res) => {
-    res.end()
-  })
+  handleInputErrors, updatePost)
 
-router.delete('/post/:id', async (req, res) => {
-  let numId = parseInt(req.params.id);
-  const post = await prisma.posts.delete({
-    where: { pid: numId }
-  });
-  return res.json(post);
-})
+router.delete('/post/:id', deletePost);
 
 // comments
-router.get('/comments/:post_id', async (req, res) => {
-  let numId = parseInt(req.params.post_id);
-  const comments = await prisma.comments.findMany({
-    where: { post_id: numId }
-  });
-  return res.json(comments);
-})
+router.get('/comments/:post_id', getComment)
 
 router.post('/comment',
   body('pid').notEmpty().isNumeric(),
   body('comment').notEmpty().isString(),
   handleInputErrors,
-  (req, res) => {
-    res.end()
-  })
+  createComment
+)
 
 router.put('/comment/:id',
   body('comment').notEmpty().isString(),
   handleInputErrors,
-  (req, res) => {
-    res.end()
-  })
+  updateComment
+)
 
-router.delete('/comment:id', async (req, res) => {
-  let numId = parseInt(req.params.id);
-  const comment = await prisma.comments.delete({
-    where: { cid: numId }
-  });
-  return res.json(comment);
-})
+router.delete('/comment:id', deleteComment)
 
 export default router
