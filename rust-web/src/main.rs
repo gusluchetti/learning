cc13e4506ec6c440c0a7fe7db54af4d6b09b5507
Gustavo Luchetti <gustavo@luchetti.dev>
2025-03-22 11:55:08 -0300

    feat: testing leptos

---
+++ rust-web/src/main.rs
@@ -0,0 +1,18 @@
+use leptos::prelude::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            *set_count.write() += 1;
+        }>"Click me: " {count}</button>
+        <p>"Double count: " {move || count.get() * 2}</p>
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+    leptos::mount::mount_to_body(App);
+}
