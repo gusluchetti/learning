16dcb076a88e5707f51246fbd8a362caa73a24dc
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2025-03-24 14:48:54 -0300

    feat: attempting to do 'ice cold beer'

--- testing-bevy/src/main.rs
+++ testing-bevy/src/main.rs
@@ -1,50 +1,41 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 #[derive(Component)]
-struct Person;
+struct Bar;
 
 #[derive(Component)]
-struct Name(String);
+struct Ball;
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
-    }
+fn setup(
+    mut commands: Commands,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(Camera3d::default())
+        .insert(Transform::from_xyz(1.0, 2.0, 17.0).looking_at(Vec3::ZERO, Vec3::Y));
+    commands
+        .spawn(Collider::cuboid(8.0, 1.0, 1.0))
+        .insert(Bar)
+        .insert(MeshMaterial3d(materials.add(Color::WHITE)))
+        .insert(Transform::from_xyz(0.0, 0.5, 0.0));
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Ball)
+        .insert(MeshMaterial3d(materials.add(Color::WHITE)))
+        .insert(Collider::ball(1.0))
+        .insert(Transform::from_xyz(0.0, 4.0, 0.0));
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Huwe".to_string())));
-    commands.spawn((Person, Name("Jamie Hannigan".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
+//TODO:
+fn move_bar() {}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup)
         .run();
 }
