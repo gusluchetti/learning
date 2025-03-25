d5422102bb48e2513d01c32703c6fb6007ae334f
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2025-03-25 20:54:09 -0300

    feat: replace insert with single spawn with tuple
    and update so app starts in borderless windowed fullscreen

--- testing-bevy/src/main.rs
+++ testing-bevy/src/main.rs
@@ -1,6 +1,7 @@
 use bevy::color::palettes::css::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::pbr::light_consts::lux::FULL_DAYLIGHT;
+use bevy::window::WindowMode;
 use bevy::{core_pipeline::prepass::DepthPrepass, prelude::*};
 use bevy_panorbit_camera::PanOrbitCamera;
 use bevy_rapier3d::prelude::*;
@@ -39,10 +40,11 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let _camera = commands
-        .spawn(PanOrbitCamera::default())
-        .insert(DepthPrepass)
-        .insert(Transform::from_xyz(0.0, 0.5, 20.0).looking_at(Vec3::ZERO, Vec3::Y));
+    let _camera = commands.spawn((
+        PanOrbitCamera::default(),
+        DepthPrepass,
+        Transform::from_xyz(0.0, 0.5, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 
     let _directional_light = commands.spawn((
         DirectionalLight {
@@ -52,74 +54,76 @@ fn setup(
         Transform::from_xyz(0.0, 4.0, 18.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
-    let _hole = commands
-        .spawn(Mesh3d(meshes.add(Cylinder::new(0.6, HOLE_SIZE))))
-        .insert(
-            Transform::from_xyz(5.0, 5.0, -HOLE_SIZE / 1.5)
-                .with_rotation(Quat::from_rotation_x(90.0)),
-        )
-        .insert(MeshMaterial3d(materials.add(StandardMaterial {
+    let _hole = commands.spawn((
+        Mesh3d(meshes.add(Cylinder::new(0.6, HOLE_SIZE))),
+        Transform::from_xyz(5.0, 5.0, -HOLE_SIZE / 1.5).with_rotation(Quat::from_rotation_x(90.0)),
+        MeshMaterial3d(materials.add(StandardMaterial {
             base_color: BLACK.into(),
             ..Default::default()
-        })))
-        .insert(Hole);
+        })),
+        Hole,
+    ));
 
     // TODO: understand depth mask to mask cylinder from wall, and add collider-only transparent
     // mesh to make ball fall off
-    let _board = commands
-        .spawn(Mesh3d(meshes.add(Cuboid::new(
-            BOARD_WIDTH,
-            BOARD_HEIGHT,
-            0.5,
-        ))))
-        .insert(Transform::from_xyz(0.0, 0.0, -1.25))
-        .insert(Collider::cuboid(BOARD_WIDTH / 2., BOARD_HEIGHT / 2., 0.25))
-        .insert(MeshMaterial3d(materials.add(StandardMaterial {
+    let _board = commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(BOARD_WIDTH, BOARD_HEIGHT, 0.5))),
+        Transform::from_xyz(0.0, 0.0, -1.25),
+        Collider::cuboid(BOARD_WIDTH / 2., BOARD_HEIGHT / 2., 0.25),
+        MeshMaterial3d(materials.add(StandardMaterial {
             base_color: YELLOW.into(),
             ..Default::default()
-        })))
-        .insert(Wall);
-
-    let left_joint = RevoluteJointBuilder::new(Vec3::Z).local_anchor1(Vec3::new(-6.0, 0.0, 0.0));
-    let _left_motor = commands
-        .spawn(RigidBody::KinematicPositionBased)
-        .insert(Transform::from_xyz(-BOARD_WIDTH / 2., 0.0, 0.0))
-        .insert(ImpulseJoint::new(bar, left_joint))
-        .insert((Motor, Position::Left));
-
-    let right_joint = RevoluteJointBuilder::new(Vec3::Z).local_anchor1(Vec3::new(6.0, 0.0, 0.0));
-    let _right_motor = commands
-        .spawn(RigidBody::KinematicPositionBased)
-        .insert(Transform::from_xyz(BOARD_WIDTH / 2., 0.0, 0.0))
-        .insert(ImpulseJoint::new(bar, right_joint))
-        .insert((Motor, Position::Right));
+        })),
+        Wall,
+    ));
 
     let bar = commands
-        .spawn(Mesh3d(meshes.add(Cuboid::new(BOARD_WIDTH, 1.0, 1.0))))
-        .insert(Transform::from_xyz(-30.0, -20.0, 0.0))
-        .insert(Collider::cuboid(BOARD_WIDTH / 2., 0.5, 0.5))
-        .insert(MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: SILVER.into(),
-            ..Default::default()
-        })))
-        .insert(RigidBody::Dynamic)
-        .insert(Sleeping::disabled())
-        .insert(Bar)
+        .spawn((
+            Mesh3d(meshes.add(Cuboid::new(BOARD_WIDTH, 1.0, 1.0))),
+            Transform::from_xyz(-30.0, -20.0, 0.0),
+            Collider::cuboid(BOARD_WIDTH / 2., 0.5, 0.5),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: SILVER.into(),
+                ..Default::default()
+            })),
+            RigidBody::Dynamic,
+            Sleeping::disabled(),
+            Bar,
+        ))
         .id();
 
-    let _ball = commands
-        .spawn(Mesh3d(meshes.add(Sphere::default().mesh())))
-        .insert(Transform::from_xyz(0.0, 2.5, 0.0))
-        .insert(Collider::ball(0.5))
-        .insert(MeshMaterial3d(materials.add(StandardMaterial {
+    let left_joint =
+        RevoluteJointBuilder::new(Vec3::Z).local_anchor1(Vec3::new(-(BOARD_WIDTH / 2.), 0.0, 0.0));
+    let _left_motor = commands.spawn((
+        Transform::from_xyz(-BOARD_WIDTH / 2., 0.0, 0.0),
+        RigidBody::KinematicPositionBased,
+        ImpulseJoint::new(bar, left_joint),
+        (Motor, Position::Left),
+    ));
+
+    let right_joint =
+        RevoluteJointBuilder::new(Vec3::Z).local_anchor1(Vec3::new(BOARD_WIDTH / 2., 0.0, 0.0));
+    let _right_motor = commands.spawn((
+        Transform::from_xyz(BOARD_WIDTH / 2., 0.0, 0.0),
+        RigidBody::KinematicPositionBased,
+        ImpulseJoint::new(bar, right_joint),
+        (Motor, Position::Right),
+    ));
+
+    let _ball = commands.spawn((
+        Mesh3d(meshes.add(Sphere::default().mesh())),
+        Transform::from_xyz(0.0, 2.5, 0.0),
+        Collider::ball(0.5),
+        MeshMaterial3d(materials.add(StandardMaterial {
             base_color: Color::srgb_from_array([192., 189., 186.]),
             metallic: 0.6,
             perceptual_roughness: 0.1,
             ..Default::default()
-        })))
-        .insert(RigidBody::Dynamic)
-        .insert(Sleeping::disabled())
-        .insert(Ball);
+        })),
+        RigidBody::Dynamic,
+        Sleeping::disabled(),
+        Ball,
+    ));
 }
 
 fn camera_follow_player(
@@ -192,7 +196,13 @@ fn handle_bar_movement(
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(FrameTimeDiagnosticsPlugin)
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
