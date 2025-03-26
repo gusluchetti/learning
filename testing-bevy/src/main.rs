b683b12e240a458f0793f7b4427d36382fe45b13
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2025-03-25 21:39:03 -0300

    wip: bar as cylinder, update ball and bar friction properties

--- testing-bevy/src/main.rs
+++ testing-bevy/src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use bevy::color::palettes::css::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::pbr::light_consts::lux::FULL_DAYLIGHT;
@@ -30,10 +32,17 @@ struct Hole;
 const MOVE_SPEED: f32 = 0.015;
 const MAX_DISTANCE: f32 = 3.0;
 const CAMERA_HEIGHT_OFFSET: f32 = 6.0;
-const HOLE_SIZE: f32 = 2.0;
 
 const BOARD_WIDTH: f32 = 20.0;
 const BOARD_HEIGHT: f32 = 40.0;
+const BOARD_DEPTH: f32 = 1.25;
+
+const BALL_RADIUS: f32 = 0.5;
+
+const BALL_BAR_FRICTION_RULE: Friction = Friction {
+    coefficient: 0.25,
+    combine_rule: CoefficientCombineRule::Min,
+};
 
 fn setup(
     mut commands: Commands,
@@ -54,21 +63,11 @@ fn setup(
         Transform::from_xyz(0.0, 4.0, 18.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
-    let _hole = commands.spawn((
-        Mesh3d(meshes.add(Cylinder::new(0.6, HOLE_SIZE))),
-        Transform::from_xyz(5.0, 5.0, -HOLE_SIZE / 1.5).with_rotation(Quat::from_rotation_x(90.0)),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: BLACK.into(),
-            ..Default::default()
-        })),
-        Hole,
-    ));
-
     // TODO: understand depth mask to mask cylinder from wall, and add collider-only transparent
     // mesh to make ball fall off
     let _board = commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(BOARD_WIDTH, BOARD_HEIGHT, 0.5))),
-        Transform::from_xyz(0.0, 0.0, -1.25),
+        Transform::from_xyz(0.0, 0.0, -BOARD_DEPTH),
         Collider::cuboid(BOARD_WIDTH / 2., BOARD_HEIGHT / 2., 0.25),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: YELLOW.into(),
@@ -77,16 +76,27 @@ fn setup(
         Wall,
     ));
 
+    let _hole = commands.spawn((
+        Mesh3d(meshes.add(Cylinder::new(BALL_RADIUS + 0.1, BOARD_DEPTH + 0.10))),
+        Transform::from_xyz(0.0, 5.0, -BOARD_DEPTH).with_rotation(Quat::from_rotation_x(PI / 2.)),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: BLACK.into(),
+            ..Default::default()
+        })),
+        Hole,
+    ));
+
     let bar = commands
         .spawn((
-            Mesh3d(meshes.add(Cuboid::new(BOARD_WIDTH, 1.0, 1.0))),
-            Transform::from_xyz(-30.0, -20.0, 0.0),
-            Collider::cuboid(BOARD_WIDTH / 2., 0.5, 0.5),
+            Mesh3d(meshes.add(Cylinder::new(0.5, BOARD_WIDTH))),
+            Transform::from_xyz(-30.0, -20.0, 0.0).with_rotation(Quat::from_rotation_z(PI / 2.)),
+            Collider::cylinder(BOARD_WIDTH / 2., 0.25),
             MeshMaterial3d(materials.add(StandardMaterial {
                 base_color: SILVER.into(),
                 ..Default::default()
             })),
             RigidBody::Dynamic,
+            BALL_BAR_FRICTION_RULE,
             Sleeping::disabled(),
             Bar,
         ))
@@ -111,9 +121,18 @@ fn setup(
     ));
 
     let _ball = commands.spawn((
-        Mesh3d(meshes.add(Sphere::default().mesh())),
+        Mesh3d(
+            meshes.add(
+                Sphere {
+                    radius: BALL_RADIUS,
+                }
+                .mesh(),
+            ),
+        ),
         Transform::from_xyz(0.0, 2.5, 0.0),
         Collider::ball(0.5),
+        BALL_BAR_FRICTION_RULE,
+        ColliderMassProperties::Mass(100.0),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: Color::srgb_from_array([192., 189., 186.]),
             metallic: 0.6,
